ccbd3ccd55f199437b3a3bd239b5a58acba4a82f
github username <github email address>
2019-01-24 10:16:02 -0500

    added search feature for favorites and recipes

--- src/components/recipeCard.js
+++ src/components/recipeCard.js
@@ -4,8 +4,8 @@ const RecipeCard = (props) => (
   <div className="recipe-card">
     <div className="recipe-card-header">{props.recipe.name}</div>
     <div className="recipe-details">
-      <p className="recipe-creator">{props.recipe.user.name}</p>
-      <p className="recipe-faves">{props.recipe.favorites.length} faves</p>
+      <p className="recipe-creator">{props.recipe.user ? props.recipe.user.name :null}</p>
+      {props.recipe.favorites ?<p className="recipe-faves"> {props.recipe.favorites.length} FAVES</p>:null}
     </div>
   </div>
 )
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,11 +2,12 @@ import React from "react"
 import {connect} from "react-redux"
 
 import NavBar from '../components/navbar'
+import RecipeCard from '../components/recipeCard'
 
 class ProfilePage extends React.Component{
   state = {
     searchTerm: '',
-    recipeType: 'All'
+    recipeType: 'all'
   }
 
   handleChange = (e) => {
@@ -15,16 +16,31 @@ class ProfilePage extends React.Component{
     })
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.searchRecipes()
+  }
 
+  searchRecipes = () => {
+    return(this.state.searchTerm === '' ? this.props.user.recipes :
+    this.props.user.recipes.filter(r => r.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())))
+  }
+
+  searchFavorites = () => {
+    console.log(this.props.user.favorites);
+    return(this.state.searchTerm === '' ? this.props.user.favorites :
+    this.props.user.favorites.filter(r => r.recipe.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())))
+  }
 
   render(){
+    console.log(this.state.searchTerm)
     return(
       <React.Fragment>
         <NavBar />
         {this.props.user ?
           <React.Fragment>
             <div className="search-sidebar">
-              <form className="search-form">
+              <form className="search-form" onSubmit={this.handleSubmit}>
                 <label htmlFor="searchTerm">Search for a recipe:</label>
                 <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange}/>
                 <select onChange={this.handleChange} name="recipeType">
@@ -32,11 +48,18 @@ class ProfilePage extends React.Component{
                   <option value="my-recipes">My Recipes</option>
                   <option value="my-favs">My Favorites</option>
                 </select>
+                <input type="submit" value="find recipes"/>
               </form>
             </div>
             <div className="recipe-results">
-              <h2>Recipes Found:</h2>
-              {this.props.user.recipes.map(r => <div style={{marginLeft: "300px"}}>r.name</div>)}
+              <h2>My Recipes:</h2>
+              <div className="recipe-container">
+                {this.props.user.recipes ? this.searchRecipes().map(r => <RecipeCard key={r.id} recipe={r}/>): null}
+              </div>
+              <h2>My Favorites:</h2>
+              <div className="recipe-container">
+                {this.props.user.favorites ? this.searchFavorites().map(r => <RecipeCard key={r.id} recipe={r.recipe}/>): <p> No Favorites Found :(</p>}
+              </div>
             </div>
           </React.Fragment>
           :
